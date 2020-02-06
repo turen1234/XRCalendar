06591014692cba70dcdf517756deb99dd20a5072
Tim Uren <timothyuren@gmail.com>
2020-02-06 18:07:11 +1100

    6/02/2020

--- Source/Extinction Rebellion/XRSyncManifest.cs
+++ Source/Extinction Rebellion/XRSyncManifest.cs
@@ -13,14 +13,12 @@ namespace Extinction_Rebellion
 		{
 			["XR VIC"] = "https://www.facebook.com/pg/XRVictoria/events/?ref=page_internal",
 			["XR AUS"] = "https://www.facebook.com/pg/XRAustralia/events/?ref=page_internal",
-			["XR NSW"] = "https://www.facebook.com/pg/xrnsw/events/?ref=page_internal",
 			["XR SEQ"] = "https://www.facebook.com/pg/extinctionrebellionseq/events/?ref=page_internal",
 			["XR RMIT"] = "https://www.facebook.com/pg/xrrmit/events/?ref=page_internal",
 			["XR Students Youth VIC"] = "https://www.facebook.com/pg/XRStudentsYouthVIC/events/?ref=page_internal",
 			["XR TAS"] = "https://www.facebook.com/pg/TazzyXR/events/?ref=page_internal",
 			["XR UK"] = "https://www.facebook.com/pg/XRebellionUK/events/?ref=page_internal",
 			["XR ACT"] = "https://www.facebook.com/pg/XRACT/events/?ref=page_internal",
-			["XR Sydney"] = "https://www.facebook.com/pg/xrsydney/events/?ref=page_internal",
 			["XR Adelaide"] = "https://www.facebook.com/pg/xradelaide/events/?ref=page_internal",
 			["XR Blue Mountains"] = "https://www.facebook.com/pg/xrbluemountains/events/?ref=page_internal",
 			["XR Woodend"] = "https://www.facebook.com/groups/XRWoodend/events/",
@@ -28,6 +26,22 @@ namespace Extinction_Rebellion
 			["XR Darebin"] = "https://www.facebook.com/pg/XRDarebin/events/?ref=page_internal",
 			["XR Macedon Ranges"] = "https://www.facebook.com/pg/XRMacedonRanges/events/?ref=page_internal",
 			["XR Gippsland"] = "https://www.facebook.com/pg/XRGippsland/events/?ref=page_internal",
+			["XR Inner-East Melbourne"] = "https://www.facebook.com/pg/xrinnereastmelb/events/?ref=page_internal",
+			["XR Port Philip"] = "https://www.facebook.com/pg/XRPortPhillip/events/?ref=page_internal",
+		};
+
+
+		public static readonly Dictionary<string, string> Sources_NSW = new Dictionary<string, string>()
+		{
+			["XR NSW"] = "https://www.facebook.com/pg/xrnsw/events/?ref=page_internal",
+			["XR Hornsby Shire"] = "https://www.facebook.com/pg/Extinction-Rebellion-Hornsby-Shire-406087173450328/events/?ref=page_internal",
+			["XR Sydney"] = "https://www.facebook.com/pg/xrsydney/events/?ref=page_internal",
+
+			["XR Inner-West Sydney"] = "https://www.facebook.com/pg/XRInnerWestSydney/events/?ref=page_internal",
+			["XR Wollongong"] = "https://www.facebook.com/pg/XRWollongong/events/?ref=page_internal",
+			["XR Southern Highlands"] = "https://www.facebook.com/pg/xrsouthernhighlands/events/?ref=page_internal",
+			["XR Central Coast"] = "https://www.facebook.com/pg/XRCentralCoast/events/?ref=page_internal",
+			["XR Newcastle"] = "https://www.facebook.com/pg/ExtinctionRebellionNewcastle/events/?ref=page_internal"
 		};
 
 
