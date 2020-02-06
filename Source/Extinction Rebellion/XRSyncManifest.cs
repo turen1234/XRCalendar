5aa53c686f2ea3587fbb67e2b6c6c412a9e26f2c
Tim Uren <timothyuren@gmail.com>
2020-02-06 17:43:30 +1100

    6/02/2020

--- Source/Extinction Rebellion/XRSyncManifest.cs
+++ Source/Extinction Rebellion/XRSyncManifest.cs
@@ -22,7 +22,12 @@ namespace Extinction_Rebellion
 			["XR ACT"] = "https://www.facebook.com/pg/XRACT/events/?ref=page_internal",
 			["XR Sydney"] = "https://www.facebook.com/pg/xrsydney/events/?ref=page_internal",
 			["XR Adelaide"] = "https://www.facebook.com/pg/xradelaide/events/?ref=page_internal",
-			["XR Blue Mountains"] = "https://www.facebook.com/pg/xrbluemountains/events/?ref=page_internal"
+			["XR Blue Mountains"] = "https://www.facebook.com/pg/xrbluemountains/events/?ref=page_internal",
+			["XR Woodend"] = "https://www.facebook.com/groups/XRWoodend/events/",
+			["XR Moreland"] = "https://www.facebook.com/pg/XRMoreland/events/?ref=page_internal",
+			["XR Darebin"] = "https://www.facebook.com/pg/XRDarebin/events/?ref=page_internal",
+			["XR Macedon Ranges"] = "https://www.facebook.com/pg/XRMacedonRanges/events/?ref=page_internal",
+			["XR Gippsland"] = "https://www.facebook.com/pg/XRGippsland/events/?ref=page_internal",
 		};
 
 
